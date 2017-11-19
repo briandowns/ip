0a6a17144076cd1933e78d672400423243ccbbfb
Brian Downs <brian.downs@gmail.com>
2017-11-19 11:56:36 -0700

    name change

---
+++ ip.go
@@ -0,0 +1,22 @@
+package ip
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func IP() {
+	res, err := http.Get("http://httpbin.org/ip")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+	var data map[string]string
+	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(data["origin"])
+}
